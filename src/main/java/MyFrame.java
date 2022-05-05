f8be5d3e8c6448d4dab0c242927d304c0b225980
Buckomd <adjordjevic94@yahoo.com>
2022-05-05 21:45:46 +0200

    Initial commit

---
+++ src/main/java/MyFrame.java
@@ -0,0 +1,89 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class MyFrame extends JFrame {
+
+    private static String tittle = "Imenik";
+    private JComboBox jComboBox;
+    private JPasswordField jPasswordField;
+    private JButton jButton;
+    private String[] username = {"buckomd", "krits"};
+
+
+
+    public MyFrame() {
+        super(tittle);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(400, 200, 400, 200);
+        setLayout(new FlowLayout());
+
+        initializationComponents();
+        addComponents();
+        addActionListener();
+    }
+
+    private void initializationComponents() {
+        jComboBox = new JComboBox(username);
+        jPasswordField = new JPasswordField(10);
+        jButton = new JButton("Login");
+    }
+
+    private void addComponents() {
+        add(jComboBox);
+        add(jPasswordField);
+        add(jButton);
+    }
+
+
+    private void addActionListener() {
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pass = String.valueOf(jPasswordField.getPassword());
+                System.out.println(pass);
+                String user = jComboBox.getSelectedItem().toString();
+
+                String server_ip = "127.0.0.1";
+                String shema = "vezbaimenik";
+                String url = "jdbc:mysql://" + server_ip + "/" + shema;
+
+
+
+
+                try(Connection connection = DriverManager.getConnection(url,"root","Aleksandar.94")) {
+
+                    System.out.println("Tu smo!");
+
+                    String sql = "SELECT * FROM vezbaimenik.korisnik WHERE kor_username LIKE '%" +user+ "%'";
+
+
+                    ResultSet rs = connection.createStatement().executeQuery(sql);
+                    System.out.println("OVDE SMo");
+
+                    while (rs.next()){
+                        String out = rs.getString("kor_username") + " " + rs.getString("kor_password");
+                        System.out.println(out);
+                    }
+
+                } catch (Exception ex) {
+                }
+
+            }
+
+        });
+    }
+
+
+    public static void main(String[] args) {
+
+        MyFrame myFrame = new MyFrame();
+        myFrame.setVisible(true);
+
+
+    }
+}
